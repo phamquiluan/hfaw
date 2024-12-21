824dde579432e44964a7cbedaffb9ec8f2005d45
Luan Pham <phamquiluan@gmail.com>
2024-12-21 13:39:15 +0000

    Move BARO to separate files

--- RCAEval/e2e/__init__.py
+++ RCAEval/e2e/__init__.py
@@ -35,7 +35,6 @@ if is_py310():
         from .causalai import causalai
     except Exception as e:
         pass
-        # print(e)
     from .circa import circa
     from .cloudranger import cloudranger
     from .fci_pagerank import fci_pagerank
@@ -53,6 +52,7 @@ if is_py310():
         pc_randomwalk,
     )
     from .run import run
+    from .baro import baro
 else:
     from .rcd import rcd
 
@@ -122,52 +122,7 @@ def nsigma(data, inject_time=None, dataset=None, num_loop=None, sli=None, anomal
     }
 
 
-def robust_scaler(
-    data, inject_time=None, dataset=None, num_loop=None, sli=None, anomalies=None, **kwargs
-):
-    if anomalies is None:
-        normal_df = data[data["time"] < inject_time]
-        anomal_df = data[data["time"] >= inject_time]
-    else:
-        normal_df = data.head(anomalies[0])
-        # anomal_df is the rest
-        anomal_df = data.tail(len(data) - anomalies[0])
 
-
-    normal_df = preprocess(
-        data=normal_df, dataset=dataset, dk_select_useful=kwargs.get("dk_select_useful", False)
-    )
-
-    anomal_df = preprocess(
-        data=anomal_df, dataset=dataset, dk_select_useful=kwargs.get("dk_select_useful", False)
-    )
-
-    # intersect
-    intersects = [x for x in normal_df.columns if x in anomal_df.columns]
-    normal_df = normal_df[intersects]
-    anomal_df = anomal_df[intersects]
-
-    ranks = []
-
-    for col in normal_df.columns:
-        a = normal_df[col].to_numpy()
-        b = anomal_df[col].to_numpy()
-
-        scaler = RobustScaler().fit(a.reshape(-1, 1))
-        zscores = scaler.transform(b.reshape(-1, 1))[:, 0]
-        score = max(zscores)
-        ranks.append((col, score))
-
-    ranks = sorted(ranks, key=lambda x: x[1], reverse=True)
-    ranks = [x[0] for x in ranks]
-
-    return {
-        "node_names": normal_df.columns.to_list(),
-        "ranks": ranks,
-    }
-
-
-baro = robust_scaler
 
 
 def e_diagnosis(
