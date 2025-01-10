4bdcbe6c6cb09519dd3170ded3a10d03edb68b68
Luan Pham <phamquiluan@gmail.com>
2025-01-10 04:10:45 +0000

    Add test_ht and test_e_diagnosis

--- tests/test_rcd.py
+++ tests/test_rcd.py
@@ -18,3 +18,19 @@ def test_rcd():
 
     # Check if the script ran successfully
     assert result.returncode == 0, f"Script failed with return code {result.returncode}\nOutput: {result.stdout}\nError: {result.stderr}"
+
+
+def test_ht():
+    command = ["python", "main-ase.py", "--method", "ht", "--dataset", "online-boutique", "--test"]
+    result = subprocess.run(command, capture_output=True, text=True)
+
+    # Check if the script ran successfully
+    assert result.returncode == 0, f"Script failed with return code {result.returncode}\nOutput: {result.stdout}\nError: {result.stderr}"
+
+
+def test_e_diagnosis():
+    command = ["python", "main-ase.py", "--method", "e_diagnosis", "--dataset", "online-boutique", "--test"]
+    result = subprocess.run(command, capture_output=True, text=True)
+
+    # Check if the script ran successfully
+    assert result.returncode == 0, f"Script failed with return code {result.returncode}\nOutput: {result.stdout}\nError: {result.stderr}"
